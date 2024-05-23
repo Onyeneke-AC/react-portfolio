36fbe860c9193db2f9cd7473eefc1d9097fbf458
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-05-23 22:56:26 +0100

    sticking the navbar to the top

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,21 @@
 import styled from "styled-components";
 import MainNav from "./MainNav";
 import Logo from "./Logo";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const StyledHeader = styled.header`
   display: flex;
   font-weight: var(--fw-bold);
   margin-bottom: 2em;
+  position: sticky;
+  top: 0;
 `;
 
 function Navbar() {
   return (
     <StyledHeader>
       <Logo />
-      <button class="nav-toggle" aria-label="toggle navigation">
-        <span class="hamburger"></span>
-      </button>
+      {/* <GiHamburgerMenu /> */}
       <MainNav />
     </StyledHeader>
   );
