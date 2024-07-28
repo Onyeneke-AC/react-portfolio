26039aeb9f5d0cf49bd4871afafe20ab7fc6b1c8
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-07-28 08:04:56 +0100

    json description updates

--- src/assets/data.js
+++ src/assets/data.js
@@ -3,9 +3,9 @@ export const portfolioData = [
     id: 1,
     title: "The Wild Oasis",
     subtitle: "Cabin Management System for Employees",
-    stack: "react, supabase, styled-components (for styling)",
+    stack: "React JS, Supabase, Styled-components (for styling)",
     description:
-      "A light and dark themed web application that allows employees to manage their cabin reservations, check-ins and check-outs. It also tracks the sales made within different date intervals. Customers booking details are stored and can be deleted if required. Optional breakfast is also an added feature to this app. Different react libraries were made use of in the development of this web app. For instance: react-router was used in handling routing between the pages, react-hook-form was used for form management etc.",
+      "A double themed (light and dark) react web application that allows employees to manage their cabin reservations, check-ins and check-outs. It also tracks the sales made within different date intervals. Customers booking details are stored and can be deleted if required. Optional breakfast is also an added feature to this app. Different react libraries were made use of in the development of this web app. For instance: react-router was used in handling routing between the pages, react-hook-form was used for form management etc. React Query was used for handling remote state management and contextAPI was used in handling UI state management. A back-end was also implemented using supabase. Supabase is an open-source backend-as-a-service platform that provides developers with a suite of tools to build and scale their applications. At its core, Supabase utilizes PostgreSQL, a powerful and extensible relational database, offering full SQL capabilities and the ability to handle complex queries with ease. Queries such as filtering, sorting and pagination were handled using both custom made components in react and supabase. Images were stored in supabase and authentication was also handled using this service.",
     mainImg: "img/1-wild-oasis",
     subImg: "img/2-wild-oasis",
     demo: "https://the-wild-oasis-sand-two.vercel.app/",
@@ -14,10 +14,10 @@ export const portfolioData = [
   {
     id: 2,
     title: "Property Pulse",
-    subtitle: "Cabin Management System for Employees",
-    stack: "react, supabase, styled-components (for styling)",
+    subtitle: "Property Rentals",
+    stack: "React JS, Tailwind CSS (for styling)",
     description:
-      "A light and dark themed web application that allows employees to manage their cabin reservations, check-ins and check-outs. It also tracks the sales made within different date intervals. Customers booking details are stored and can be deleted if required. Optional breakfast is also an added feature to this app. Different react libraries were made use of in the development of this web app. For instance: react-router was used in handling routing between the pages, react-hook-form was used for form management etc.",
+      "A fullstack web application that displays different rental properties in the US. It also displays the different prices of these retail together with certain details about each properties. Properties can be bookmarked once the user is logged in. ContextAPI was used in handling global state management.Authentication and authorization was done using next-auth where google was the provider used. Nextjs is a full stack framework, for front end it implements react code and turns it into magic with server side rendering, incremental static regeneration etc. It also boosts the seo performance. For routing nextjs uses filesystem as routes, and for backend, nextjs uses nodejs in app/api directory and subdirectories. Other than that nextjs has some components that optimizes the end product, like next/image or next/script. MongoDB was the database used in this project and it was connected to this project using mongoose. Cloudinary was used to store images. Geographical mapping was an added feature and this was implemented using mapbox. React libraries such as react-share, react-photoswipe-gallery etc. were also used in implementing added features to this project.",
     mainImg: "img/1-property-pulse",
     subImg: "img/2-property-pulse",
     demo: "https://property-pulse-flax.vercel.app/",
@@ -26,10 +26,10 @@ export const portfolioData = [
   {
     id: 3,
     title: "Fast React Pizza Co.",
-    subtitle: "Cabin Management System for Employees",
-    stack: "react, supabase, styled-components (for styling)",
+    subtitle: "Pizza Ordering System",
+    stack: "React JS, Tailwind CSS (for styling)",
     description:
-      "A light and dark themed web application that allows employees to manage their cabin reservations, check-ins and check-outs. It also tracks the sales made within different date intervals. Customers booking details are stored and can be deleted if required. Optional breakfast is also an added feature to this app. Different react libraries were made use of in the development of this web app. For instance: react-router was used in handling routing between the pages, react-hook-form was used for form management etc.",
+      "A simple react web application used for ordering pizza. A pizza-order api deployed on render was used to get the pizzas available on the menu, create and update an order and then save the orders. The data fetching capabilities of react-router-dom was explored and this was used to fetch and post data for the entirety of this project. Tailwind CSS was used for the styling of this project. Redux was used for state management where slices were created for two different features of the app: the cart and the user.",
     mainImg: "img/1-fast-react-pizza",
     subImg: "img/2-fast-react-pizza",
     demo: "https://fast-react-pizza-eight-gilt.vercel.app/",
