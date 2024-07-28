be5e143809f690eb7670be22df0fc32fececf620
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-07-28 02:32:39 +0100

    creating the json data for the portfolio

---
+++ src/assets/data.js
@@ -0,0 +1,74 @@
+export const portfolioData = [
+  {
+    id: 1,
+    title: "The Wild Oasis",
+    subtitle: "Cabin Management System for Employees",
+    stack: "react, supabase, styled-components (for styling)",
+    description:
+      "A light and dark themed web application that allows employees to manage their cabin reservations, check-ins and check-outs. It also tracks the sales made within different date intervals. Customers booking details are stored and can be deleted if required. Optional breakfast is also an added feature to this app. Different react libraries were made use of in the development of this web app. For instance: react-router was used in handling routing between the pages, react-hook-form was used for form management etc.",
+    mainImg: "img/1-wild-oasis",
+    subImg: "img/2-wild-oasis",
+    demo: "https://the-wild-oasis-sand-two.vercel.app/",
+    github: "https://github.com/Onyeneke-AC/the-wild-oasis",
+  },
+  {
+    id: 2,
+    title: "Property Pulse",
+    subtitle: "Cabin Management System for Employees",
+    stack: "react, supabase, styled-components (for styling)",
+    description:
+      "A light and dark themed web application that allows employees to manage their cabin reservations, check-ins and check-outs. It also tracks the sales made within different date intervals. Customers booking details are stored and can be deleted if required. Optional breakfast is also an added feature to this app. Different react libraries were made use of in the development of this web app. For instance: react-router was used in handling routing between the pages, react-hook-form was used for form management etc.",
+    mainImg: "img/1-property-pulse",
+    subImg: "img/2-property-pulse",
+    demo: "https://property-pulse-flax.vercel.app/",
+    github: "https://github.com/Onyeneke-AC/property-pulse",
+  },
+  {
+    id: 3,
+    title: "Fast React Pizza Co.",
+    subtitle: "Cabin Management System for Employees",
+    stack: "react, supabase, styled-components (for styling)",
+    description:
+      "A light and dark themed web application that allows employees to manage their cabin reservations, check-ins and check-outs. It also tracks the sales made within different date intervals. Customers booking details are stored and can be deleted if required. Optional breakfast is also an added feature to this app. Different react libraries were made use of in the development of this web app. For instance: react-router was used in handling routing between the pages, react-hook-form was used for form management etc.",
+    mainImg: "img/1-fast-react-pizza",
+    subImg: "img/2-fast-react-pizza",
+    demo: "https://fast-react-pizza-eight-gilt.vercel.app/",
+    github: "https://github.com/Onyeneke-AC/fast-react-pizza",
+  },
+  {
+    id: 4,
+    title: "Tenece Website Clone",
+    subtitle: "Cabin Management System for Employees",
+    stack: "react, supabase, styled-components (for styling)",
+    description:
+      "A light and dark themed web application that allows employees to manage their cabin reservations, check-ins and check-outs. It also tracks the sales made within different date intervals. Customers booking details are stored and can be deleted if required. Optional breakfast is also an added feature to this app. Different react libraries were made use of in the development of this web app. For instance: react-router was used in handling routing between the pages, react-hook-form was used for form management etc.",
+    mainImg: "img/1-tenece",
+    subImg: "img/2-tenece",
+    demo: "https://tenece-landing-page-react.vercel.app/",
+    github: "https://github.com/Onyeneke-AC/Tenece_Landing_Page_React",
+  },
+  {
+    id: 5,
+    title: "Adopt a pet",
+    subtitle: "Cabin Management System for Employees",
+    stack: "react, supabase, styled-components (for styling)",
+    description:
+      "A light and dark themed web application that allows employees to manage their cabin reservations, check-ins and check-outs. It also tracks the sales made within different date intervals. Customers booking details are stored and can be deleted if required. Optional breakfast is also an added feature to this app. Different react libraries were made use of in the development of this web app. For instance: react-router was used in handling routing between the pages, react-hook-form was used for form management etc.",
+    mainImg: "img/1-adopt-me",
+    subImg: "img/2-adopt-me",
+    demo: "https://adopt-me-ecru-five.vercel.app/",
+    github: "https://github.com/Onyeneke-AC/Adopt-me",
+  },
+  {
+    id: 6,
+    title: "Simple Weather App",
+    subtitle: "Cabin Management System for Employees",
+    stack: "react, supabase, styled-components (for styling)",
+    description:
+      "A light and dark themed web application that allows employees to manage their cabin reservations, check-ins and check-outs. It also tracks the sales made within different date intervals. Customers booking details are stored and can be deleted if required. Optional breakfast is also an added feature to this app. Different react libraries were made use of in the development of this web app. For instance: react-router was used in handling routing between the pages, react-hook-form was used for form management etc.",
+    mainImg: "img/1-weather",
+    subImg: "img/2-weather",
+    demo: "https://weather-app-mauve-beta.vercel.app/",
+    github: "https://github.com/Onyeneke-AC/weather-app",
+  },
+];
