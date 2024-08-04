d8a9b8899cb5512ee2588642fe2cd128d68b78ef
Onyeneke Anthony Chidubem <98026459+Onyeneke-AC@users.noreply.github.com>
2024-08-04 04:41:01 +0100

    routing fix complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AppLayout from "./components/AppLayout";
-import Home from "./pages/Home";
 import Project from "./pages/Project";
 import "./index.css";
 import PageNotFound from "./pages/PageNotFound";
@@ -10,9 +9,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route index element={<AppLayout />} />
-        {/* <Route index element={<Navigate replace to="home" />} />
-          <Route path="home" element={<Home />} /> */}
-        {/* </Route> */}
         <Route path="project/:projectId" element={<Project />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -1,4 +1,3 @@
-import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import styled from "styled-components";
